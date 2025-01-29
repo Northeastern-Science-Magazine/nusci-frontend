0d5ff6e37df950fa5324f7f09d1d9dbdcf347c2e
eekansh-M <eekansh.muppasani@outlook.com>
2025-01-29 00:31:37 -0500

    Fixing final styling changes
    Adding padding to the card and fixing final style changes such as card size.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,11 +8,12 @@ interface CardProps {
   // Card Component
   export default function Card({ imageURL, teamTitle, teamDescription }: CardProps) {
     return (
-      <div className="max-w-md bg-white rounded-lg border-gray-200 drop-shadow-2xl w-2/5 h-1/2">
+      <div className="max-w-lg bg-white rounded-lg border-gray-200 drop-shadow-2xl  h-1/2">
         <div className="p-6 flex justify-stretch">
           <div className="team-title/description">
           <h3 className="">{teamTitle}</h3>
-          <p className="text-[#757575] mb-3">{teamDescription}</p> 
+          <div className="p-1"></div>
+          <p className="text-[#757575]">{teamDescription}</p> 
           </div>
           <img src={imageURL} className="size-40"/>
         </div>

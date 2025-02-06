90c1a55c1c9318e2a16206db8501c8ed0546bbdc
yurikakan <kan.yu@northeastern.edu>
2025-02-05 19:39:53 -0500

    req method fix
    added field so one can specify what req method they want to use

--- src/api/hook.ts
+++ src/api/hook.ts
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 
 // returns any type T
-const useFetch = <T>(url: string) => {
+// takes in url, options object including request method, headers, body, settings
+const useFetch = <T>(url: string, options: RequestInit = {}) => {
     // returns any type T, initially empty
     const [data, setData] = useState<T | null>(null);
 
     useEffect(() => {
         // fetch data function
         const fetchData = async () => {
-            const response = await fetch(url);
+            // specifies request method
+            const response = await fetch(url, options);
             const data = await response.json();
             setData(data);
         }
@@ -19,3 +21,4 @@ const useFetch = <T>(url: string) => {
 };
 
 export default useFetch;
+//example use: const useGet = <T>(url: string) => useFetch<T>(url, { method: 'GET' });

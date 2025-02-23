bea29b026869f5f72b13c96404ebaa2428b84d43
greatestblueheron <s.xu.nature@gmail.com>
2025-02-23 14:43:19 -0500

    adjusted for concision

--- src/components/Avatar/AvatarProps.ts
+++ src/components/Avatar/AvatarProps.ts
@@ -1,5 +1,5 @@
 export interface AvatarProps {
     src?: string;
-    alt?: string;
-    fallback: string;
+    alt?: string; // add default alt text when not given
+    fallback: string; // validate if it's 1-2 characters long in prop
 }

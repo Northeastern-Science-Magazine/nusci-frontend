d3dd88e12e69a53db44af7d1a17fe1c10adbaa8b
stephanie <stephaniechen028@gmail.com>
2025-02-25 14:30:06 -0500

    altered text sizes

--- src/components/Avatar/variants.ts
+++ src/components/Avatar/variants.ts
@@ -5,9 +5,9 @@ export const avatarVariants = tv({
   base: "rounded-full",
   variants: {
     size: {
-      sm: "w-24 h-24 text-sm",
-      md: "w-32 h-32 text-base",
-      lg: "w-40 h-40 text-lg",
+      sm: "w-24 h-24 text-lg",
+      md: "w-32 h-32 text-xl",
+      lg: "w-40 h-40 text-2xl",
     },
   }, 
   defaultVariants: {

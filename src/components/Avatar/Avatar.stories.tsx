498216650ea35a47de880cffc5bc446e4374148b
stephanie <stephaniechen028@gmail.com>
2025-03-19 19:19:52 -0400

    add gallery

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Avatar } from "./Avatar";
+import React from "react";
 
 
 /* Modify this when adding variants to Avatar */
@@ -22,18 +23,42 @@ type Story = StoryObj<typeof Avatar>;
 
 /** Story for Default Avatar */
 export const Default: Story = {
-    args: {
-        src: "https://cdn.britannica.com/73/9173-050-9D9EA4BA/Surgeonfish.jpg",
-        alt: "a blue surgeonfish avatar",
-        fallback: "SX",
-    }
+  args: {
+    src: "https://cdn.britannica.com/73/9173-050-9D9EA4BA/Surgeonfish.jpg",
+    alt: "a blue surgeonfish avatar",
+    fallback: "SX",
+  }
 }
 
 /** Story for Text Avatar */
 export const TextAvatar: Story = {
   args: {
-      src: "",
-      alt: undefined,
-      fallback: "SXU",
+    src: "",
+    alt: undefined,
+    fallback: "SXU",
   }
 }
+
+/** Gallery Story for all Avatar Variants */
+export const Gallery: Story = {
+  args: {},
+  render: (args) => {
+    return (
+      <div>
+        {sizes.map((size) => {
+          return (
+            <div key={size} className="p-1">
+                <Avatar
+                  src="https://cdn.britannica.com/73/9173-050-9D9EA4BA/Surgeonfish.jpg"
+                  alt="a blue surgeonfish avatar"
+                  fallback="SX"
+                  size={size}
+                >
+                </Avatar>
+            </div>
+          );
+        })}
+      </div>
+    );
+  },
+};

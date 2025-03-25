a9fcda51f87f37682397093aff4d6eb5719e6521
yurikakan <kan.yu@northeastern.edu>
2025-03-25 02:56:54 -0400

    final changes

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,3 +1,27 @@
 // this file is for adding functions that call user-related endpoints
- 
-import { useFetch } from "./hook";
+
+import { useState, useEffect } from "react";
+
+// returns any type T
+// takes in url, options object including request method, headers, body, settings
+type HttpMethod = "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
+
+const useFetch = <T>(url: string, options: RequestInit = {}) => {     
+    // returns any type T, initially empty
+    const [data, setData] = useState<T | null>(null);
+    useEffect(() => {
+        // fetch data function
+        const fetchData = async () => {
+            // specifies request method
+            const response = await fetch(url, options);
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    }, [url]);
+    return data;
+};
+
+export default useFetch;
+
+//example use: const useGet = <T>(url: string) => useFetch('https://api.example.com/data', { method: 'POST'});

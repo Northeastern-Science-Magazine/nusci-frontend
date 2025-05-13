6a8e5b3643ae8432c7e7da908ad32477800bb27f
suttonspindler <suttonspindler@gmail.com>
2025-05-13 06:12:02 -0700

    Update Toggle.tsx

--- src/design-system/primitives/Toggle/Toggle.tsx
+++ src/design-system/primitives/Toggle/Toggle.tsx
@@ -7,10 +7,10 @@ export const Toggle = React.forwardRef<React.ElementRef<typeof Switch.Root>, Tog
   ({ className, variant = "black", size = "md", label, labelClassName, ...props }, ref) => {
     return (
       <label className="flex items-center gap-2 cursor-pointer">
+        <span className={`text-sm font-medium ${labelClassName || ""}`}>{label}</span>
         <Switch.Root ref={ref} className={toggleVariants({ variant, size, className })} {...props}>
           <Switch.Thumb className="block rounded-full bg-white transition-transform duration-100 will-change-transform my-0.5" />
         </Switch.Root>
-        <span className={`text-sm font-medium ${labelClassName || ""}`}>{label}</span>
       </label>
     );
   }

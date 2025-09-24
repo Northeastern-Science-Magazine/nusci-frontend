83b8a4b62fbe5ec8d14ca9c4e010612dce40e601
Ethan Szeto <ethan.szeto@gmail.com>
2025-09-24 18:13:41 -0400

    changes

---
+++ src/design-system/primitives/Form/Form.stories.tsx
@@ -0,0 +1,16 @@
+// import
+
+// const meta: Meta<typeof Checkbox> = {
+//   component: Checkbox,
+//   title: "Primitives/Checkbox",
+//   argTypes: {
+//     size: {
+//       control: "select",
+//       options: sizes,
+//     },
+//     color: {
+//       control: "select",
+//       options: colors,
+//     },
+//   },
+// };
---
+++ src/design-system/primitives/Form/index.tsx
@@ -0,0 +1,1 @@
+import { Form, FormField } from "./Form";

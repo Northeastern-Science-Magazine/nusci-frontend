1feebb7881c69f7c6a589665be7617101725dbf9
Ethan Szeto <ethan.szeto@gmail.com>
2026-01-21 16:10:28 -0500

    Flex and Grid subcomponents now have refined controls

--- src/design-system/primitives/Grid/Grid.tsx
+++ src/design-system/primitives/Grid/Grid.tsx
@@ -1,14 +1,13 @@
-import { GridColProps, GridProps, GridRowProps, gridColVariants, gridRowVariants, gridVariantsCN } from "./variants";
-import clsx from "clsx";
+import { GridColProps, GridProps, GridRowProps, gridColVariantsCN, gridRowVariantsCN, gridVariantsCN } from "./variants";
 
 export function Grid({ className, children, ...props }: GridProps) {
   return <div className={gridVariantsCN(props, className)}>{children}</div>;
 }
 
 export function GridCol({ className, children, ...variantProps }: GridColProps) {
-  return <div className={clsx(gridColVariants(variantProps), className)}>{children}</div>;
+  return <div className={gridColVariantsCN(variantProps, className)}>{children}</div>;
 }
 
 export function GridRow({ className, children, ...variantProps }: GridRowProps) {
-  return <div className={clsx(gridRowVariants(variantProps), className)}>{children}</div>;
+  return <div className={gridRowVariantsCN(variantProps, className)}>{children}</div>;
 }
